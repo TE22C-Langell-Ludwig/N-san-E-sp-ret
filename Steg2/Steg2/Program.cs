e8010e334f2872d82a34082b2a963ad4a1170c66
Ludwig Langell <ludwig.langell@elev.ga.ntig.se>
2024-11-21 10:57:59 +0100

    Steg2

---
+++ Steg2/Steg2/Program.cs
@@ -0,0 +1,24 @@
+﻿List<String> Cities = new List<String>();
+
+Console.WriteLine("säg några städer, Säg exit när du är klar");
+while (true)
+{
+string newcity = Console.ReadLine();
+if(newcity == "exit")
+{
+break;
+}
+Cities.Add(newcity);
+}
+Console.Clear();
+Console.WriteLine("här är alla städer du just nämnde");
+for (int i = 0; i < Cities.Count; i++)
+{
+Console.WriteLine($"{Cities[i]}");
+}
+Console.ReadLine();
+/*
+Skapa en tom string-lista som heter "cities". Check
+Skapa en loop där du varje gång loopen körs hämtar en string från användaren med Console.ReadLine. Lägg till den string du får från användaren till cities-listan. Avbryt loopen om användaren skriver "exit". Check
+Skapa en loop som ligger efter den ovanstående. Den här loopen ska skriva ut alla strings som finns sparade i cities-listan. Check
+*/
